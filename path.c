cb8879f212bcf0d54302882df6f7d4a12bf83b8e
jordanlouie1 <jordanlouie.1@gmail.com>
2017-07-23 13:28:10 -0400

    adding queries

--- path.c
+++ path.c
@@ -3,6 +3,17 @@ graph *Load_Graph(char *filename, graph *input)
 	return input;
 }
 queries *Load_Queries(char *filename){
+	FILE * in = fopen(filename, "-r");
+
+	int size;
+
+	fscanf("%d", size);
+
+	queries * input = malloc(sizeof(queries) * (size+1));
+
+	for(int i = 0; i < size; i++){
+	}
+
 	return input;
 }
 
