06c5a7ceb2476cbf68ee8809839c309529a3d0a6
Carson-Kelley <kelley96@purdue.edu>
2017-07-24 16:42:43 -0400

    Still working on distance

--- main.c
+++ main.c
@@ -14,10 +14,10 @@ int main(int argc, char **argv)
 	graph *map = malloc(sizeof(graph));
 	map = Load_Graph(argv[1], map);
 
-    int test;
+    /*int test;
 	queries *tests = Load_Queries(argv[2], &test);
 
-	FreeQueries(tests);
+	FreeQueries(tests);*/
 	FreeGraph(map);
 
 	return EXIT_SUCCESS;
