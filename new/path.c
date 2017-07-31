9c3919370b298aea4e3b3c98994f9a02d81b41f5
jordanlouie1 <jordanlouie.1@gmail.com>
2017-07-30 20:42:09 -0400

    maybe free

--- new/path.c
+++ new/path.c
@@ -12,7 +12,18 @@ void FreeQueries(queries *test)
 
 void FreeGraph(graph *map)
 {
-	
+	int i = 0;
+	free(map->nodes);
+	for(i = 0; (i< map->vertices); i = i + 1){
+		while(map->nodes[i]->edges != NULL){
+			edge * freeE = map->nodes[i]->edges;
+			map->nodes[i]->edges = map->nodes[i]->edges->next;
+			free(freeE);	
+		}
+		i = i + 1;
+	}
+	free(map->nodes);
+	free(map);	
 }
 
 int Find_Dist(node a, node b){
@@ -280,6 +291,7 @@ void dijkstras(graph *map, int start, int end)
 			}
 			currEdge = currEdge->next;
 		}
+		free(currList);
 	}
 
 	if(curr->weight == INT_MAX)
